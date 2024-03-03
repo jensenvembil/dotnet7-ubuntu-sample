d507f513f3ef0f687e94f8cd4379f322460e48ea
jensenvembil <jensenvs3@gmail.com>
2024-03-03 22:14:14 +0530

    added tracing activity of graphana

--- dotnet-7-sample-webapi/Program.cs
+++ dotnet-7-sample-webapi/Program.cs
@@ -1,3 +1,7 @@
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOpenTelemetryTracing(
+    (builder) => builder
+                   .SetResourceBuilder(
+              ResourceBuilder.CreateDefault().AddService("dotnet-7-webapi"))
+                    .AddAspNetCoreInstrumentation()
+                    .AddConsoleExporter());
 
 var app = builder.Build();
 
